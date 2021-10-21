e4359444149bf308dc05748d3ad6a4507603e5c3
wholebuzz <www.whole.buzz@gmail.com>
2021-10-21 13:18:37 -0700

    Add streamToKnex

--- src/knex.ts
+++ src/knex.ts
@@ -1,8 +1,12 @@
 import Knex from 'knex'
+import through2 from 'through2'
 import StreamTree, { ReadableStreamTree } from 'tree-stream'
 import { StreamRowsOptions } from './dump'
+import { InsertRowsOptions } from './restore'
 
-export function streamKnexRows(
+const batch2 = require('batch2')
+
+export function streamFromKnex(
   knex: Knex,
   query: Knex.QueryBuilder,
   options?: StreamRowsOptions
@@ -18,4 +22,31 @@ export function streamKnexRows(
     query = query.where([options?.versionKey || 'updated_at', '>', options.version])
   }
   return StreamTree.readable(query.stream())
+}
+
+export function streamToKnex(
+  source: {
+    knex?: Knex
+    transaction?: Knex.Transaction
+  },
+  options: InsertRowsOptions
+) {
+  const stream = StreamTree.writable(
+    through2.obj(function (data: any[], _: string, callback: () => void) {
+      let query = source.transaction
+        ? source.transaction.batchInsert(options.table, data)
+        : source.knex!.batchInsert(options.table, data)
+      if (options.returning) query = query.returning(options.returning)
+      if (source.transaction) query = query.transacting(source.transaction)
+      query
+        .then((result) => {
+          if (options.returning) this.push(result)
+          callback()
+        })
+        .catch((err) => {
+          throw err
+        })
+    })
+  )
+  return stream.pipeFrom(batch2.obj({ size: options.batchSize ?? 4000 }))
 }
