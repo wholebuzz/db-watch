586aaf4ad0ad17c3acd9e9c4bd8435283d6a1784
wholebuzz <www.whole.buzz@gmail.com>
2021-10-28 16:04:54 -0700

    Add streamToKnexRaw

--- src/knex.ts
+++ src/knex.ts
@@ -1,3 +1,12 @@
+import byline from 'byline'
+import {
+  defaultSplitterOptions,
+  mssqlSplitterOptions,
+  mysqlSplitterOptions,
+  postgreSplitterOptions,
+  sqliteSplitterOptions,
+} from 'dbgate-query-splitter/lib/options'
+import { SplitQueryStream } from 'dbgate-query-splitter/lib/splitQueryStream'
 import Knex from 'knex'
 import through2 from 'through2'
 import StreamTree, { ReadableStreamTree } from 'tree-stream'
@@ -49,4 +58,49 @@ export function streamToKnex(
     })
   )
   return stream.pipeFrom(batch2.obj({ size: options.batchSize ?? 4000 }))
+}
+
+export function streamToKnexRaw(
+  source: {
+    knex?: Knex
+    transaction?: Knex.Transaction
+  },
+  options?: { returning?: boolean }
+) {
+  let stream = StreamTree.writable(
+    through2.obj(function (data: string, _: string, callback: () => void) {
+      const text = data.replace(/\?/g, '\\?')
+      const query = source.transaction ? source.transaction.raw(text) : source.knex!.raw(text)
+      query
+        .then((result) => {
+          if (options?.returning) this.push(result)
+          callback()
+        })
+        .catch((err) => {
+          throw err
+        })
+    })
+  )
+  stream = stream.pipeFrom(
+    newStatementSplitterStream(
+      ((source.transaction || source.knex) as any).context.client.config.client
+    )
+  )
+  stream = stream.pipeFrom(byline.createStream())
+  return stream
+}
+
+export function newStatementSplitterStream(type?: any) {
+  switch (type) {
+    case 'postgresql':
+      return new SplitQueryStream(postgreSplitterOptions)
+    case 'mysql':
+      return new SplitQueryStream(mysqlSplitterOptions)
+    case 'mssql':
+      return new SplitQueryStream(mssqlSplitterOptions)
+    case 'sqlite':
+      return new SplitQueryStream(sqliteSplitterOptions)
+    default:
+      return new SplitQueryStream(defaultSplitterOptions)
+  }
 }
